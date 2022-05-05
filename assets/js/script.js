4e597e82ace343b0424b6cca09993a7348fcb5d4
Ellie Sargent <elliecsargent@gmail.com>
2022-05-05 18:15:39 -0500

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -219,7 +219,20 @@ var deleteTask = function(taskId) {
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+};
+
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
+        return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+    for (var i = 0; i < savedTasks.length; i++) {
+        createTaskEl(savedTasks[i]);
+    }
+};
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
